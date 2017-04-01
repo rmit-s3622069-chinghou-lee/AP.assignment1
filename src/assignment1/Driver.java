ccfb932a7229fd49e29d6e5ed3c2694b4945aa0a
Theodore <Theodore@LAPTOP-15QHHEEJ>
2017-04-01 13:27:46 +1100

    Progress: Tidy code according to requirement

--- src/assignment1/Driver.java
+++ src/assignment1/Driver.java
@@ -3,9 +3,7 @@ package assignment1;
 import java.util.Scanner;
 
 public class Driver {
-	private String gameType;
-	private Game game; // Ozylympic's game
-	
+
 	// constants corresponding to main menu options
 	private static final int gameSelect = 1;
 	private static final int gamePrediction = 2;
@@ -13,59 +11,135 @@ public class Driver {
 	private static final int displayFinalResult = 4;
 	private static final int displayAthletePoints = 5;
 	private static final int gameExit = 6;
-	
-	public Driver(){
-		game = new Game(); // create game
-	} // end no-argument Driver constructor
 
-	public int displayMenu() { // display the Ozlympic's main menu and return an input selection
+	public void menuSelect() {
+
+		int option = displayMenu();
+
+		switch (option) {
+		case gameSelect:
+			gameSelect();
+			displayMenu();
+			break;
+		case gamePrediction:
+			gamePrediction();
+			break;
+		case gameStart:
+			gameStart();
+			break;
+		case displayFinalResult:
+			displayFinalResult();
+			break;
+		case displayAthletePoints:
+			displayAthletePoints();
+			break;
+		case gameExit:
+			System.out.println("Game Over!");
+			System.exit(0);
+			break;
+		}
+	}
+
+	public int displayMenu() { // display the Ozlympic's main menu and return an
+								// input selection
 		int option = 0;
 		boolean validInput = false;
-		
+
 		do {
-			try{
-			String menu[] = { "Ozlympic Game", "========================", "1. Select a game to run",
-					"2. Predict the winner of the game", "3. Start the game",
-					"4. Display the final results of all games", "5. Display the points of all athletes", "6. Exit" };
-			for (int i = 0; i < menu.length; i++)
-				System.out.println(menu[i]); // display the main menu
-			
-			
-			System.out.print("Enter a option: ");
-			Scanner scanner = new Scanner(System.in);
-			option = scanner.nextInt(); // user insert input
-			if (option >= 1 && option <= 6) { // include try catch
-				System.out.println("");
-				validInput = true;
-			} else {
-				System.out.println("Please insert a valid input!");
-				System.out.println("");
-				validInput = false;
-			}
-			}catch (Exception e){
+			try {
+				String menu[] = { "Ozlympic Game", "========================", "1. Select a game to run",
+						"2. Predict the winner of the game", "3. Start the game",
+						"4. Display the final results of all games", "5. Display the points of all athletes",
+						"6. Exit" };
+				for (int i = 0; i < menu.length; i++)
+					System.out.println(menu[i]); // display the main menu
+
+				System.out.print("Enter a option: ");
+				Scanner scanner = new Scanner(System.in);
+				option = scanner.nextInt(); // user insert input
+				if (option >= 1 && option <= 6) {
+					System.out.println("");
+					validInput = true;
+				} else {
+					System.out.println("Please insert a valid input!");
+					System.out.println("");
+					validInput = false;
+				}
+			} catch (Exception e) {
 				System.out.println("Not a valid Input. Please try again!");
 				System.out.println("");
 				validInput = false;
 			}
 		} while (!validInput);
-		
+
 		return option; // return user's selection
 	} // end method displayMenu
 
-	public void menuSelect() {
-		
-		// local variable to store currentGame currently being processed
-		Game currentGame = null;
-		
-		int option = displayMenu();
-		switch (option) {
-		case gameSelect: 			game.gameSelect(); break;
-		case gamePrediction: 		game.gamePrediction(); break;
-		case gameStart: 			game.gameStart(); break;
-		case displayFinalResult: 	game.displayFinalResult(); break;
-		case displayAthletePoints: 	game.displayAthletePoints(); break;
-		case gameExit: 				System.out.println("Game Over!"); System.exit(0); break;
-		}
+	public int gameSelect() {
+		int option = 0;
+		boolean validInput = false;
+
+		do {
+			try {
+				String menu[] = { "Select a sport to play: ", "1. Swimming race", "2. Running race",
+						"3. Cycling race" };
+				for (int i = 0; i < menu.length; i++)
+					System.out.println(menu[i]); // display the main menu
+
+				System.out.print("Enter a option: ");
+				Scanner scanner = new Scanner(System.in);
+				option = scanner.nextInt(); // user insert input
+
+				if (option == 1) {
+					validInput = true;
+				} else if (option == 2) {
+					validInput = true;
+				} else if (option == 3) {
+					validInput = true;
+				} else {
+					System.out.println("Please insert a valid input!");
+					System.out.println("");
+					validInput = false;
+				}
+			} catch (Exception e) {
+				System.out.println("Not a valid Input. Please try again!");
+				System.out.println("");
+				validInput = false;
+			}
+		} while (!validInput);
+		return option;
+	}
+
+	public int gamePrediction() {
+		int userPredict = 0;
+		int option = gameSelect();
+		boolean validInput = false;
+		do {
+			try {
+				// race.printAthletes(option);
+				System.out.println("Please predict the winner by entering the athlete's ID: ");
+				Scanner scanner = new Scanner(System.in);
+				userPredict = scanner.nextInt();
+			} catch (Exception e) {
+				System.out.println("Please insert valid athlete's ID!");
+			}
+		} while (!validInput);
+
+		return userPredict;
+	}
+
+	public void gameStart() {
+		System.out.println("gameStart");
+
+	}
+
+	public void displayFinalResult() {
+		System.out.println("displayFinalResult");
+	}
+
+	public void displayAthletePoints() {
+		System.out.println("displayAthletePoints");
+
 	}
 
 }
--- src/assignment1/Game.java
+++ src/assignment1/Game.java
@@ -4,92 +4,10 @@ import java.util.Scanner;
 
 public class Game {
 
-	private String raceID;
+	private int raceID;
+	private String raceType;
 
-	private int minAthletes;
-	private int maxAthletes;
-	private int minTime;
-	private int maxTime;
-
-	private ParticipantsDatabase pd;
-
-	public Game() {
-	pd = new ParticipantsDatabase(raceID, raceID, maxAthletes, raceID, raceID);
-	}
-
-	public String gameSelect() {
-		int option = 0;
-		String raceType = null;
-		boolean validInput = false;
-
-		do {
-			try {
-				String menu[] = { "Select a sport to play: ", "1. Swimming race", "2. Running race",
-						"3. Cycling race" };
-				for (int i = 0; i < menu.length; i++)
-					System.out.println(menu[i]); // display the main menu
-
-				System.out.print("Enter a option: ");
-				Scanner scanner = new Scanner(System.in);
-				option = scanner.nextInt(); // user insert input
-
-				if (option == 1) {
-					raceType = "swim";
-					//pd.getParticipantType(raceType);
-					validInput = true;
-				} else if (option == 2) {
-					raceType = "run";
-					//pd.getParticipantType(raceType);
-					validInput = true;
-				} else if (option == 3) {
-					raceType = "cycle";
-					//pd.getParticipantType(raceType);
-					validInput = true;
-				} else {
-					System.out.println("Please insert a valid input!");
-					System.out.println("");
-					validInput = false;
-				}
-			} catch (Exception e) {
-				System.out.println("Not a valid Input. Please try again!");
-				System.out.println("");
-				validInput = false;
-			}
-		} while (!validInput);
-		// System.out.println(raceType);
-		return raceType;
-	}
-
-	public int gamePrediction() {
-		int userPredict = 0;
-		boolean validInput = false;
-		do {
-			try {
-				System.out.println("Print out athletes base on gameType!");
-				System.out.println("Please predict the winner by entering the athlete's ID: ");
-				Scanner scanner = new Scanner(System.in);
-				userPredict = scanner.nextInt();
-			} catch (Exception e) {
-				System.out.println("Please insert valid athlete's ID!");
-			}
-		} while (!validInput);
-
-		return userPredict;
-	}
-
-	public void gameStart() {
-		//System.out.println("gameStart");
-		pd.printParticipant();
-	}
-
-	public void displayFinalResult() {
-		System.out.println("displayFinalResult");
-
-	}
-
-	public void displayAthletePoints() {
-		System.out.println("displayAthletePoints");
-
+	public Game(int raceID) {
+		this.raceID = raceID;
 	}
 }
-
